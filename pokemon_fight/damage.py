f8741fcb67a778cbee0d23d72121e02478185e45
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-10-31 17:36:03 +0100

    more realistic damage formula

--- pokemon_fight/damage.py
+++ pokemon_fight/damage.py
@@ -2,9 +2,10 @@ def deal_damage(attacker, target):
     # compute attack side of the damage formula
     move = attacker.next_move
     if move.kind.capitalize() == "Physical":
-        attack_power = move.base_power * attacker.attack
+        # Let's use int to get a whole number
+        attack_power = move.base_power * attacker.attack / 100
     if move.kind.capitalize() == "Special":
-        attack_power = move.base_power * attacker.special_attack
+        attack_power = move.base_power * attacker.special_attack / 100
     # apply STAB (same type attack bonus)
     if move.type == attacker.type:
         attack_power = attack_power * 1.5
@@ -16,8 +17,10 @@ def deal_damage(attacker, target):
         defense_power = target.special_defense
 
     # damage formula
-    damage = attack_power * (1 - defense_power / 200)
+    damage = int(attack_power * (1 - defense_power / 200))
     # deal the damage
     target.current_hp -= damage
     # print some feedback for the user
-    print(f"{target.name} took {'{:.0%}'.format(damage/target.max_hp)} of its total HP as damage.")
+    print(
+        f"{target.name} took damage: {'{:.0%}'.format(damage/target.max_hp)} of its total HP."
+    )
